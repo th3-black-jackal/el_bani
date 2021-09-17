c4c972a1b67e52d55ad3ad0e43fabd29ef6aee6b
nasser <abdelnasserahmed96@gmail.com>
2021-09-17 11:21:15 +0200

    Added generate forms shell, to generate forms from UIs files

---
+++ generate_form_code.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+app_name=$1
+form_name=$2
+
+ROOT_PATH=`pwd`
+APP_PATH="$ROOT_PATH/$1"
+FORMS_PATH="$APP_PATH/forms/uis"
+PYTHON_FORMS_PATH="$APP_PATH/forms/python_forms"
+
+python_form_name=$form_name
+python_form_name=${python_form_name/".ui"/".py"}
+form_path="$FORMS_PATH/$form_name"
+
+python_form_path="$PYTHON_FORMS_PATH/$python_form_name"
+pyuic5 $form_path -o $python_form_path
